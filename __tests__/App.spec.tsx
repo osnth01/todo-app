c796f87fe9f1680a3d67f854fece5976b29bcc74
osnth01 <osnth01@gmail.com>
2018-05-10 23:23:29 -0400

    create App component

---
+++ __tests__/App.spec.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react'
+import { createRenderer } from 'react-test-renderer/shallow'
+import App from '../src/App'
+
+const setup = () => {
+  const renderer = createRenderer()
+  renderer.render(<App />)
+  const output = renderer.getRenderOutput()
+  return output
+}
+
+describe('App', () => {
+  it('should say "To Do"', () => {
+    const output = setup()
+    expect(output.props.children).toEqual('To Do')
+  })
+})
--- src/index.tsx
+++ src/index.tsx
@@ -1,1 +1,8 @@
-console.log('hi')
+import * as React from 'react'
+import { render } from 'react-dom'
+
+import App from './App'
+
+const root = document.getElementById('root')
+
+render(<App />, root)
